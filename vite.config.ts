3db2353205d792675ded5f38fc7edb18eb353ea4
amirulsymml <amirulsymml@gmail.com>
2026-04-22 01:55:31 +0800

    fix: adding build in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/ATS-cv-generator-vue/',
+  build: {
+    outDir: 'dist', // Output directory
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html') // Entry point for the build
+    }
+  }
 })
